51525ea4aaabec617b888efb1dded5f98fdcc7cc
bmcaldarella <cal23025@byui.edu>
2024-04-30 20:58:10 +0100

    commit

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,17 @@
+public class Job
+{
+    public string _jobTitle;
+    public string _company;
+    public int _startYear;
+    public int _endYear;
+
+    public void DisplayJobDetails(string jobTitle, string company, int startYear, int endYear)
+    {
+        _jobTitle = jobTitle;
+        _company = company;
+        _startYear = startYear;
+        _endYear = endYear;
+
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+}
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,22 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1.DisplayJobDetails("Software Engineer", "Microsoft", 2017, 2022);
+
+        Job job2 = new Job();
+        job2.DisplayJobDetails("Manager", "Apple", 2022, 2023);
+
+        Resume resume1 = new Resume();
+
+        resume1._name = "Brandon Caldarella";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.Display();
     }
 }
