5331440dc28aab45ff02661c5cce5e1945928dd1
bmcaldarella <cal23025@byui.edu>
2024-05-25 14:39:27 +0100

    commit

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("Red", 4);
+        Rectangle rectangle = new Rectangle("Blue", 3, 6);
+        Circle circle = new Circle("Green", 5);
+
+        List<Shape> shapes = new List<Shape>
+        {
+            square,
+            rectangle,
+            circle
+        };
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}, Area: {shape.GetArea()}");
+        }
     }
 }
